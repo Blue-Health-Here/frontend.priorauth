a3a2159203194bf846ed2d01f9b36a55e1c1956e
Mehreen47 <mehreen@bluehealthhere.com>
2025-04-10 01:44:56 +0500

    homepage: hero section responsivenes, some ui fixes
    separate common component of button
    
    homepage: created about section with responsivenes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
+import HomeLayout from "../../layouts/HomeLayout";
+import AboutSection from "./AboutSection";
 import HeroSection from "./HeroSection";
 
 const Home = () => {
   return (
-    <>
-      <HeroSection />
-    </>
+    <HomeLayout>
+      <div
+        className="relative w-full md:min-h-screen bg-cover bg-center flex flex-col justify-between pt-20 overflow-hidden"
+        style={{
+          backgroundImage: `url(${"/images/herosection-bg-image.jpg"})`,
+        }}
+      >
+        <HeroSection />
+      </div>
+      <AboutSection />
+    </HomeLayout>
   );
 };
 
