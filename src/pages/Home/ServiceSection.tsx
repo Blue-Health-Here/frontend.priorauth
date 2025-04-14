3cba7dc083b20a1cfe4b64c2381716aae78c44be
mohsinbluehealth <mohsin@bluehealthhere.com>
2025-04-14 18:50:38 +0500

    fix navigation arrow button to set the next and previous event

--- src/pages/Home/ServiceSection.tsx
+++ src/pages/Home/ServiceSection.tsx
@@ -58,13 +58,11 @@ const ServiceSection: React.FC = () => {
     <div className="w-full pl-8 py-32">
       <div className="grid mb-12 grid-cols-12">
         <div className="col-span-1">
-          <span className="text-primary-black font-secondary text-base md:text-lg">
-            What We Offer?
-          </span>
+          <span className="text-primary-black font-secondary text-base md:text-lg">What We Offer?</span>
         </div>
         <div className="col-span-1"></div>
-        <div className="col-span-8 text-primary-black ">
-          <h2 className="text-xl sm:text-3xl md:text-5xl ">Our Services</h2>
+        <div className="col-span-8 text-primary-black">
+          <h2 className="text-xl sm:text-3xl md:text-5xl">Our Services</h2>
           <p className="font-secondary text-sm md:text-base">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
             turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
@@ -75,17 +73,14 @@ const ServiceSection: React.FC = () => {
         <div className="col-span-1"></div>
         <div className="col-span-1">
           <div className="flex gap-x-4">
-             
             <img
-              src="/images/chevronleft.svg"
-              alt=""
-              onClick={() => sliderRef.current?.slickNext()}
+              src="/images/chevronleft.svg" alt=""
+              onClick={() => sliderRef.current?.slickPrev()}
               className="p-2 rounded-2xl border border-tertiary-black hover:bg-quaternary-black"
             />
-           <img
-              src="/images/chevronright.svg"
-              alt=""
-              onClick={() => sliderRef.current?.slickPrev()}
+            <img
+              src="/images/chevronright.svg" alt=""
+              onClick={() => sliderRef.current?.slickNext()}
               className="p-2 rounded-2xl border border-tertiary-black hover:bg-quaternary-black"
             />
           </div>
