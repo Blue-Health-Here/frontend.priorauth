25636793f892c53acdeb9753e78f7f9f4a78301a
Mehreen47 <mehreen@bluehealthhere.com>
2025-04-15 19:38:23 +0500

    fix text sizes responsiveness

--- src/pages/Home/CreateIdeaSection.tsx
+++ src/pages/Home/CreateIdeaSection.tsx
@@ -3,7 +3,7 @@ import Button from '../../components/Button/Button';
 
 const CreateIdeaSection: React.FC = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center text-primary-white text-center px-6 py-20 md:py-48 relative">
+    <div className="w-full flex flex-col items-center justify-center text-primary-white text-center px-6 md:px-8 py-20 md:py-48 relative">
       <div
         className="absolute inset-0 z-0 bg-cover bg-center"
         style={{
@@ -12,14 +12,14 @@ const CreateIdeaSection: React.FC = () => {
       <div className="relative">
         <p className="text-base md:text-lg mb-2">Get An Idea Of The Application</p>
 
-        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl font-semibold mb-10">
+        <h1 className="text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl font-semibold mb-10">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Etiam eu turpis molestie, dictum est a, mattis tellus. Sed
           dignissim, metus nec fringilla accumsan, risus sem
           sollicitudin lacus, ut interdum tellus elit sed risus.
         </h1>
 
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl md:max-w-4xl mx-auto mb-10">
+        <p className="text-base sm:text-xl md:text-2xl lg:max-w-4xl mx-auto mb-10">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis
           molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla
           accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.
