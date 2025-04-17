82ba082a5f57f8f015af7469491c3e00baeccbce
mohsinbluehealth <mohsin@bluehealthhere.com>
2025-04-17 22:21:14 +0500

    fix lint errors

--- src/pages/Home/ServiceSection.tsx
+++ src/pages/Home/ServiceSection.tsx
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { services } from "../../constants";
-import FooterSection from "./FooterSection";
 
 const ServiceSection: React.FC = () => {
   const sliderRef = useRef<Slider>(null);
