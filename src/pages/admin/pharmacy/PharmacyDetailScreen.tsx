08e824d46d9189f9af44a355fc41da1bfe26edbb
mohsinbluehealth <mohsin@bluehealthhere.com>
2025-05-23 00:14:18 +0500

    integrate pharmacy details screen API

--- src/pages/admin/pharmacy/PharmacyDetailScreen.tsx
+++ src/pages/admin/pharmacy/PharmacyDetailScreen.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import DataTable from '../../../components/common/DataTable'
 import PharmacyDetailsCrad from './PharmacyDetailsCrad'
 import { rquestDetailpageData } from '../../../utils/constants'
+import { useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { fetchPharmacyDetails } from '../../../services/adminService'
 
 const PharmacyDetailScreen: React.FC = () => {
+  const [pharmacyDetails, setPharmacyDetails] = useState(null);
+  const { pharmacyId } = useParams();
+  const isPharmacyDetailFetched = useRef(false);
+  const dispatch = useDispatch();
+  // console.log(pharmacyId, "params");
+
+  const fetchData = async () => {
+    const response = await fetchPharmacyDetails(dispatch, pharmacyId); 
+    setPharmacyDetails(response);
+  }
+
+  useEffect(() => {
+    if (!isPharmacyDetailFetched.current) {
+      fetchData();
+      isPharmacyDetailFetched.current = true;
+    }
+  }, []);
+
   return (
     <div className="shadow-lg rounded-b-2xl">
-      <PharmacyDetailsCrad />
+      <PharmacyDetailsCrad details={pharmacyDetails} />
       <DataTable
         className="rounded-b-2xl rounded-t-none"
         title="Requests"
