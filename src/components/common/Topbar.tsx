ed80c2586926edadf2e04bb1560a8e8979d0e36f
syedaiqrazawar <129755923+syedaiqrazawar@users.noreply.github.com>
2025-06-27 18:49:55 +0500

    topbar-updated

--- src/components/common/Topbar.tsx
+++ src/components/common/Topbar.tsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import NavbarProfileDropdown from './NavbarProfileDropdown';
-import { handleLogout } from '../../services/authService';
-import { getPageTitle } from '../../utils/getPageTitle';
-import { FaBars } from 'react-icons/fa6';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../store';
-import { setIsSidebarOpen } from '../../store/features/global/globalSlice';
-import NavbarNotificationDropdown from './NavbarNotificationDropdown';
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import NavbarProfileDropdown from "./NavbarProfileDropdown";
+import { handleLogout } from "../../services/authService";
+import { getPageTitle } from "../../utils/getPageTitle";
+import { FaBars } from "react-icons/fa6";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { setIsSidebarOpen } from "../../store/features/global/globalSlice";
+import NavbarNotificationDropdown from "./NavbarNotificationDropdown";
 
 const Topbar: React.FC = () => {
   const { isSidebarOpen } = useSelector((state: RootState) => state.global);
-  const [isNotifDropdownOpen, setIsNotifDropdownOpen] = useState<boolean>(false);
+  const [isNotifDropdownOpen, setIsNotifDropdownOpen] =
+    useState<boolean>(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const notifDropdownRef = useRef<HTMLDivElement>(null);
@@ -20,26 +21,34 @@ const Topbar: React.FC = () => {
   const dispatch = useDispatch();
 
   const handleClickOutside = (event: MouseEvent) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
       setIsDropdownOpen(false);
     }
-    if (notifDropdownRef.current && !notifDropdownRef.current.contains(event.target as Node)) {
+    if (
+      notifDropdownRef.current &&
+      !notifDropdownRef.current.contains(event.target as Node)
+    ) {
       setIsNotifDropdownOpen(false);
     }
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
   return (
-    <div className={`fixed top-0 left-0 lg:left-[250px] xl:left-[280px] right-0 p-4 sm:p-3.5 z-60 bg-white border-b border-gray-100`}>
+    <div
+      className={`fixed top-0 left-0 lg:left-[250px] xl:left-[280px] right-0 p-3 sm:p-3 z-60 bg-white border-b border-gray-100`}
+    >
       <nav className={`flex justify-between items-center w-full`}>
-        <div className='flex items-center'>
-          <p className="text-lg sm:text-xl lg:text-2xl font-semibold text-primary-black whitespace-nowrap">
+        <div className="flex items-center">
+          <p className="text-xs scale-70 font-medium sm:text-xl text-primary-black whitespace-nowrap">
             {getPageTitle(location.pathname)}
           </p>
         </div>
@@ -47,48 +56,91 @@ const Topbar: React.FC = () => {
           <input
             type="text"
             placeholder="Search here"
-            className="w-[180px] lg:w-[220px] xl:w-[280px] 2xl:w-[350px] pl-9 pr-3 py-1.5 sm:py-2 border border-medium-stroke rounded-lg text-sm focus:outline-none placeholder:text-tertiary-white"
+            className="w-[400px] pl-10 pr-4 py-2.5 border-2 border-gray-100 rounded-lg text-sm focus:outline-none placeholder:text-[#9E9E9E] h-[40px] "
+            style={{ lineHeight: "1.5" }}
           />
-          <span className="absolute left-2.5 top-1/2 transform -translate-y-1/2 text-gray-500">
-            <img src="/search-icon.svg" alt="search icon" />
+          <span className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500">
+            <img src="/search-icon.svg" alt="search icon" className="w-4 h-4" />
           </span>
         </div>
-        {<div className="flex justify-end items-center gap-4">
-          <div className='relative' ref={notifDropdownRef}>
-            <button type='button' onClick={() => setIsNotifDropdownOpen(!isNotifDropdownOpen)} 
-              className="md:flex hidden cursor-pointer rounded-lg border border-light-stroke items-center p-2 sm:p-2.5">
-              <p className='relative'>
-                <img src="/notifications.svg" alt="notification" className='w-4 h-4 sm:h-6 sm:w-6' />
-                <span className='absolute rounded-full w-2 h-2 bg-error-clip top-0.5 right-0.5'></span>
-              </p>
-            </button>
-            {isNotifDropdownOpen && <NavbarNotificationDropdown />}
-          </div>
-          <div className='px-1 hidden lg:block rounded-lg border border-quaternary-navy-blue bg-quaternary-navy-blue' ref={dropdownRef}>
-            <div className="flex gap-3 items-center relative cursor-pointer" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
-              <img src="/images/profile-image.png" alt="Profile" className='w-7 h-7 sm:w-8 sm:h-8 md:w-10 md:h-10 rounded-full' />
-              <div className='hidden sm:block py-0.5'>
-                <h2 className='text-primary-black font-bold text-sm md:text-base'>John Doe</h2>
-                <p className='text-secondary-black text-xs md:text-sm'>johndoe@mail.com</p>
+        {
+          <div className="flex justify-end items-center gap-3.5">
+            <div className="relative" ref={notifDropdownRef}>
+              <button
+                type="button"
+                onClick={() => setIsNotifDropdownOpen(!isNotifDropdownOpen)}
+                className="md:flex hidden cursor-pointer rounded-lg border border-light-stroke items-center p-2 py-1 sm:p-2.5"
+              >
+                <p className="relative">
+                  <img
+                    src="/notifications.png"
+                    alt="notification"
+                    className="w-4 h-4 sm:h-5 sm:w-5"
+                  />
+                </p>
+              </button>
+              {isNotifDropdownOpen && <NavbarNotificationDropdown />}
+            </div>
+            <div
+              className="pl-1.5 pr-0.5 hidden lg:block rounded-lg border border-quaternary-navy-blue bg-quaternary-navy-blue"
+              ref={dropdownRef}
+            >
+              <div
+                className="pl- pr-0.5 hidden lg:block rounded-lg border border-quaternary-navy-blue bg-quaternary-navy-blue"
+                ref={dropdownRef}
+              >
+                <div
+                  className="flex gap-1 items-center relative cursor-pointer"
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                >
+                  <img
+                    src="/images/profile-image.png"
+                    alt="Profile"
+                    className="w-7 h-7 sm:w-8 sm:h-8 rounded-full"
+                  />
+                  <div className="hidden sm:block py-0.5">
+                    <h2 className="text-primary-black font-semibold text-xs">
+                      John Doe
+                    </h2>
+                    <p className="text-secondary-black text-xs scale-80 -ml-2">
+                      johndoe@mail.com
+                    </p>
+                  </div>
+                  <img
+                    src="/profile_chevron_down.svg"
+                    alt="Toggle dropdown"
+                    className="w-4 h-4 ml-[-4px]" // Added negative margin here
+                  />
+                </div>
+                {isDropdownOpen && (
+                  <NavbarProfileDropdown
+                    onClose={() => {
+                      handleLogout();
+                      navigate("/login");
+                    }}
+                  />
+                )}
               </div>
-              <img
-                src='/profile_chevron_down.svg'
-                alt='Toggle dropdown'
-                className='w-4 h-4 sm:w-5 sm:h-5'
+              {isDropdownOpen && (
+                <NavbarProfileDropdown
+                  onClose={() => {
+                    handleLogout();
+                    navigate("/login");
+                  }}
+                />
+              )}
+            </div>
+            <div
+              className="lg:hidden block"
+              onClick={() => dispatch(setIsSidebarOpen(!isSidebarOpen))}
+            >
+              <FaBars
+                size={21}
+                className="text-primary-sky-blue block lg:hidden"
               />
             </div>
-            {isDropdownOpen && (
-              <NavbarProfileDropdown onClose={() => {
-                handleLogout();
-                navigate("/login");
-              }} />
-            )}
           </div>
-          
-          <div className="lg:hidden block" onClick={() => dispatch(setIsSidebarOpen(!isSidebarOpen))}>
-            <FaBars size={22} className='text-primary-sky-blue block lg:hidden' />
-          </div>
-        </div>}
+        }
       </nav>
     </div>
   );
--- src/components/common/sidebar.tsx
+++ src/components/common/sidebar.tsx
@@ -1,18 +1,25 @@
-import React, { useEffect } from 'react';
-import { Link, NavLink, useLocation } from 'react-router-dom';
-import { adminSidebarItems, pharmacySidebarItems } from '../../utils/constants';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../store';
-import { setIsSidebarCollapsed, setIsSidebarOpen } from '../../store/features/global/globalSlice';
-import { RxCross2 } from 'react-icons/rx';
+import React, { useEffect } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { adminSidebarItems, pharmacySidebarItems } from "../../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store";
+import {
+  setIsSidebarCollapsed,
+  setIsSidebarOpen,
+} from "../../store/features/global/globalSlice";
+import { RxCross2 } from "react-icons/rx";
 
 const Sidebar: React.FC = () => {
   const location = useLocation();
   const pathName = location.pathname;
   const dispatch = useDispatch();
-  const { isSidebarCollapsed, isSidebarOpen } = useSelector((state: RootState) => state.global);
-  const sidebarItems = pathName.startsWith("/pharmacy") ? pharmacySidebarItems : adminSidebarItems;
-  
+  const { isSidebarCollapsed, isSidebarOpen } = useSelector(
+    (state: RootState) => state.global
+  );
+  const sidebarItems = pathName.startsWith("/pharmacy")
+    ? pharmacySidebarItems
+    : adminSidebarItems;
+
   useEffect(() => {
     if (window.innerWidth < 1024) {
       dispatch(setIsSidebarOpen(false));
@@ -21,50 +28,73 @@ const Sidebar: React.FC = () => {
 
   useEffect(() => {
     if (isSidebarOpen) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = '';
+      document.body.style.overflow = "";
     }
 
     return () => {
-      document.body.style.overflow = '';
+      document.body.style.overflow = "";
     };
   }, [isSidebarOpen]);
 
   const handleSidebarCollapse = () => {
     dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
-  }
+  };
 
   const asideClass = isSidebarOpen
-    ? 'max-w-full min-w-full transition-all duration-500 shadow-[0px 0px 12px 0px rgba(0, 0, 0, 0.04)] xl:min-w-[280px] xl:max-w-[280px] block text-secondary-black fixed top-0 bottom-0 z-[99]'
-    : 'max-w-[250px] min-w-[250px] transition-all duration-500 shadow-[0px 0px 12px 0px rgba(0, 0, 0, 0.04)] xl:min-w-[280px] xl:max-w-[280px] hidden lg:flex text-secondary-black flex-col fixed top-0 bottom-0 z-[99]';
+    ? "max-w-full min-w-full transition-all duration-500 shadow-[0px 0px 12px 0px rgba(0, 0, 0, 0.04)] xl:min-w-[280px] xl:max-w-[280px] block text-secondary-black fixed top-0 bottom-0 z-[99]"
+    : "max-w-[250px] min-w-[250px] transition-all duration-500 shadow-[0px 0px 12px 0px rgba(0, 0, 0, 0.04)] xl:min-w-[280px] xl:max-w-[280px] hidden lg:flex text-secondary-black flex-col fixed top-0 bottom-0 z-[99]";
 
   return (
     <>
-      {isSidebarOpen && <div className="fixed inset-0 z-[98] bg-black bg-opacity-50 lg:hidden" />}
+      {isSidebarOpen && (
+        <div className="fixed inset-0 z-[98] bg-black bg-opacity-50 lg:hidden" />
+      )}
       <aside className={asideClass}>
-        <div className='p-4 flex min-h-[79px] border-b border-gray-100 bg-primary-white justify-between items-center border-r border-gray-100'>
-          <Link to='/' className='pl-2'>
-            <img src="/images/logo.svg" alt="PriorAuth Logo" className="h-7 sm:h-8 lg:h-8" />
+        <div className="p-4 flex h-[68px] border-b bg-primary-white justify-between items-center border-r border-gray-100">
+          <Link to="/" className="pl-2">
+            <img
+              src={isSidebarCollapsed ? "/Group.svg" : "/images/logo.svg"}
+              alt="PriorAuth Logo"
+              className="h-7 sm:h-8 lg:h-8"
+            />
           </Link>
-          {!isSidebarOpen ? <img onClick={handleSidebarCollapse} src='/header-left-logo-arrow.svg' alt='header left logo arrow' className={`w-8 h-8 bg-gray-100 p-2 rounded-lg cursor-pointer ${isSidebarCollapsed ? 'scale-[-1]' : ''}`} /> : (
-            <span onClick={() => dispatch(setIsSidebarOpen(false))} className="cursor-pointer ">
+          {!isSidebarOpen ? (
+            <img
+              onClick={handleSidebarCollapse}
+              src="/sidebar.svg"
+              alt="header left logo arrow"
+              className={`w-9 h-9 p-2 rounded-lg cursor-pointer ${
+                isSidebarCollapsed ? "scale-[-1]" : ""
+              }`}
+            />
+          ) : (
+            <span
+              onClick={() => dispatch(setIsSidebarOpen(false))}
+              className="cursor-pointer"
+            >
               <RxCross2 size={20} />
             </span>
           )}
         </div>
-        <div className={`${isSidebarCollapsed ? ' !max-w-[80px] !min-w-[80px]' : ''} h-[calc(100%-79px)] 
+        <div
+          className={`${
+            isSidebarCollapsed ? " !max-w-[80px] !min-w-[80px]" : ""
+          } h-[calc(100%-79px)] 
           overflow-y-auto bg-primary-white duration-500 transition-all flex flex-col flex-1 gap-4
-          px-4 py-10 border-r border-gray-100`}>
+          px-4 py-10 border-r border-gray-100`}
+        >
           <ul className="flex flex-col gap-y-2 text-[15px]">
             {sidebarItems.map((item, index) => (
               <NavLink
                 to={item.path}
                 key={index}
                 className={({ isActive }) =>
-                  `group flex items-center gap-x-3 p-3 h-9 md:h-11 rounded-lg cursor-pointer transition font-secondary ${isActive
-                    ? 'bg-primary-sky-blue text-primary-white'
-                    : 'hover:bg-primary-sky-blue hover:text-primary-white'
+                  `group flex items-center gap-x-3 p-3 h-9 md:h-11 rounded-lg cursor-pointer transition font-secondary ${
+                    isActive
+                      ? "bg-primary-sky-blue text-primary-white"
+                      : "hover:bg-primary-sky-blue hover:text-primary-white"
                   }`
                 }
               >
@@ -74,10 +104,17 @@ const Sidebar: React.FC = () => {
                       src={item.icon}
                       alt={`${item.name} Icon`}
                       title={item.name}
-                      className={`transition duration-200 ${isActive ? 'brightness-0 invert' : 'group-hover:brightness-0 group-hover:invert'
-                        }`}
+                      className={`transition duration-200 ${
+                        isActive
+                          ? "brightness-0 invert"
+                          : "group-hover:brightness-0 group-hover:invert"
+                      }`}
                     />
-                    <span className={`${isSidebarCollapsed ? 'hidden' : 'inline'} text-xs sm:text-sm xl:text-base transition-colors duration-200 group-hover:text-primary-white`}>
+                    <span
+                      className={`${
+                        isSidebarCollapsed ? "hidden" : "inline"
+                      } text-xs sm:text-sm xl:text-base transition-colors duration-200 group-hover:text-primary-white`}
+                    >
                       {item.name}
                     </span>
                   </>
@@ -85,33 +122,47 @@ const Sidebar: React.FC = () => {
               </NavLink>
             ))}
           </ul>
-          <div className='mt-auto'>
+          <div className="mt-auto">
             <NavLink
-              to={'/admin/settings'}
+              to={"/admin/settings"}
               className={({ isActive }) =>
-                `group flex items-center gap-x-3 p-3 h-9 md:h-11 rounded-lg cursor-pointer transition font-secondary ${isActive
-                  ? 'bg-primary-sky-blue text-primary-white'
-                  : 'hover:bg-primary-sky-blue hover:text-primary-white'
+                `group flex items-center gap-x-3 p-3 h-9 md:h-11 rounded-lg cursor-pointer transition font-secondary ${
+                  isActive
+                    ? "bg-primary-sky-blue text-primary-white"
+                    : "hover:bg-primary-sky-blue hover:text-primary-white"
                 }`
               }
             >
               {({ isActive }) => (
                 <>
                   <img
-                    src={'/sidebar-Settings.svg'}
+                    src={"/sidebar-Settings.svg"}
                     alt={`Settings Icon`}
-                    className={`transition duration-200 ${isActive ? 'brightness-0 invert' : 'group-hover:brightness-0 group-hover:invert'
-                      }`}
+                    className={`transition duration-200 ${
+                      isActive
+                        ? "brightness-0 invert"
+                        : "group-hover:brightness-0 group-hover:invert"
+                    }`}
                   />
-                  <span className={`${isSidebarCollapsed ? 'hidden' : 'inline'} text-xs sm:text-sm xl:text-base transition-colors duration-200 group-hover:text-primary-white`}>
+                  <span
+                    className={`${
+                      isSidebarCollapsed ? "hidden" : "inline"
+                    } text-xs sm:text-sm xl:text-base transition-colors duration-200 group-hover:text-primary-white`}
+                  >
                     Settings
                   </span>
                 </>
               )}
             </NavLink>
-            <div className={`${isSidebarCollapsed ? 'hidden' : ''} bg-quaternary-navy-blue p-4 rounded-lg flex flex-wrap gap-2 mt-4`}>
-              <img src='/copyright.svg' alt='copyright' className='w-4 h-4' />
-              <p className='text-tertiary-navy-blue text-xs word-wrap'>Copyrights Futuro 2025 - All Rights Reserved.</p>
+            <div
+              className={`${
+                isSidebarCollapsed ? "hidden" : ""
+              } bg-quaternary-navy-blue p-4 rounded-lg flex flex-wrap gap-2 mt-4`}
+            >
+              <img src="/copyright.svg" alt="copyright" className="w-4 h-4" />
+              <p className="text-tertiary-navy-blue text-xs word-wrap">
+                Copyrights Futuro 2025 - All Rights Reserved.
+              </p>
             </div>
           </div>
         </div>
