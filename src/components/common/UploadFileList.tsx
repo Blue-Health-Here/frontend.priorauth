db34dac37b18003b2723a34409f28c3e47c4ea05
mohsinbluehealth <mohsin@bluehealthhere.com>
2025-07-01 22:50:37 +0500

    hot fix in upload file list component

--- src/components/common/UploadFileList.tsx
+++ src/components/common/UploadFileList.tsx
@@ -9,7 +9,7 @@ interface UploadFileListProps {
     isAddTags?: boolean
 }
 
-const UploadFileList: React.FC<UploadFileListProps> = ({ files, removeFile, handleAddTag, isAddTags }) => (
+const UploadFileList: React.FC<UploadFileListProps> = ({ files, removeFile, handleAddTag, isAddTags = true }) => (
     <div className="space-y-3 overflow-y-auto max-h-96">
         {files.map((file) => (
             <UploadFileItem key={file.id} file={file} removeFile={(id) => removeFile(id)} handleAddTag={handleAddTag} isAddTags={isAddTags} />
