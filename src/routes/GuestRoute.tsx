c0d53c735f3465d380b9038a76cc9d63fd3fafc4
mohsinbluehealth <mohsin@bluehealthhere.com>
2025-07-02 19:08:43 +0500

    integrate auth login api with new server url
    update axios instance with new url in configuration
    
    create middleware for protected routes and guest routes
    
    handle redirect login for authentication and based on role
    
    update app file with new formatting and by adding new routes middleware to update all routes

---
+++ src/routes/GuestRoute.tsx
@@ -0,0 +1,31 @@
+// src/routes/GuestRoute.tsx
+import { Navigate, Outlet } from "react-router-dom";
+
+type User = {
+  roleCode: string;
+};
+
+const GuestRoute = () => {
+  const storedUser = localStorage.getItem("user");
+  let user: User | null = null;
+
+  try {
+    if (storedUser) {
+      user = JSON.parse(storedUser);
+    }
+  } catch (error) {
+    console.error("Error parsing user from localStorage", error);
+  }
+
+  if (user?.roleCode === "companyAdmin") {
+    return <Navigate to="/admin/dashboard" replace />;
+  }
+
+  if (user?.roleCode === "pharmacyUser") {
+    return <Navigate to="/pharmacy/dashboard" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default GuestRoute;
